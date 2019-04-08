130a9cf09b5b8f57ff2eb85f80bb41b56b22cdb4
YuliyaPriymak <bogatir.yuliya@gmail.com>
2019-04-08 10:10:26 +0300

    first commit

---
+++ MoGo/js/my_script.js
@@ -0,0 +1,7 @@
+mySearch.onclick = function myFunction() {
+	var x = document.getElementById("searchBlock");
+
+	if(x.className === "search-block") {
+		x.className += "visual";
+	} else { x.ckassName = "search-block"; }
+}
